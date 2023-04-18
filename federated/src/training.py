216b718949798e5912827d95174fd555c85c57b5
Wei-Tung Hsu <victoria0925@gmail.com>
2023-04-18 03:15:45 +0000

    Upload training.py

---
+++ federated/src/training.py
@@ -0,0 +1,52 @@
+import copy
+import numpy as np
+from tqdm import tqdm
+
+from tensorboardX import SummaryWriter
+
+from options import args_parser
+from update import test_inference, ASRLocalUpdate
+from models import Data2VecAudioForCTC, DataCollatorCTCWithPadding
+from utils import get_dataset, average_weights, exp_details
+
+from transformers import Data2VecAudioConfig, Wav2Vec2Processor
+
+def client_train(args, model_in_path_root, model_out_path, train_dataset, 
+                 test_dataset, idx, epoch, global_weights=None):                      # train function for each client, train from model in model_in_path 
+                                                                                      #                                                                    + "_global/final/"
+                                                                                      #                                                                    + "_client" + str(idx) + "_round" + str(args.epochs-1) + "/final/"
+                                                                                      # or model in last round
+                                                                                      # final result in model_out_path + "_client" + str(client_id) + "_round" + str(global_round)
+    logger = SummaryWriter('../logs')
+    # BUILD MODEL for every process
+    if epoch == 0:                                                                    # start from global model
+        if args.STAGE == 0:                                                           # train ASR
+            model_in_path = model_in_path_root + "_global/final/"                     # using global trained ASR + AD
+        else:                                                                         # train AD or toggling network
+            model_in_path = model_in_path_root + "_client" + str(idx) + "_round" + str(args.epochs-1) + "/final/"
+                                                                                      # using final local model from last training
+    else:                                                                             # start from previous local model
+        model_in_path = model_out_path + "_client" + str(idx) + "_round" + str(epoch-1) + "/final/"
+    
+    local_model = ASRLocalUpdate(args=args, dataset=train_dataset, logger=logger, global_test_dataset=test_dataset, 
+                                 client_id=idx, model_in_path=model_in_path, model_out_path=model_out_path)
+                                                                                      # initial dataset of current client
+
+    w, loss = local_model.update_weights(global_weights=global_weights, global_round=epoch) 
+                                                                                      # from model_in_path model, update certain part using given weight
+    
+
+    return w, loss
+
+def centralized_training(args, model_in_path, model_out_path, 
+                         train_dataset, test_dataset, epoch):                         # train function for global models, train from model in model_in_path
+                                                                                      # final result in model_out_path + "_global/final"
+    logger = SummaryWriter('../logs')
+
+    local_model = ASRLocalUpdate(args=args, dataset=train_dataset, logger=logger,
+                        global_test_dataset=test_dataset, client_id="public", 
+                        model_in_path=model_in_path, model_out_path=model_out_path)   # initial public dataset
+    
+    w, loss = local_model.update_weights(global_weights=None, global_round=epoch)     # from model_in_path to train
+
+    return w, loss
