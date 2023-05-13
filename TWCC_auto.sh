8a0576301b2bd197c2483dff9ec817ec3be92369
JackingChen <stu94116@gapp.nthu.edu.tw>
2023-05-13 09:10:20 +0000

    Upload New File

---
+++ TWCC_auto.sh
@@ -0,0 +1,17 @@
+TWCC_CLI_CMD=/home/jack/anaconda3/bin/twccli
+#<USERNAME>：主機帳號
+
+echo "1. 開啟instance"
+twccli mk ccs -gpu 1 -img pytorch-23.02-py3:latest -itype PyTorch -ptype c.super -wait
+
+
+ID=$(twccli ls ccs | awk '/\|/ {print $2}' | awk 'NR==2')
+info=$(twccli ls ccs $ID -gssh)
+echo "ssh $info"
+#設定SSH Without Password
+#ssh-keygen -t rsa -n 4096
+#ssh-copy-id u1157393@203-145-216-222.ccs.twcc.ai -p 50866
+# passwd : BIIClab713isbest
+ssh -o StrictHostKeyChecking=no $info
+
+
