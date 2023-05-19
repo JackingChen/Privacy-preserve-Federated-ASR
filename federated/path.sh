9f8bdf1d9671a4a3aa51a0cfe1826f937ae86f60
JackingChen <stu94116@gapp.nthu.edu.tw>
2023-05-19 16:25:41 +0800

    20230519

--- federated/path.sh
+++ federated/path.sh
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # 宣告 DACS_codeRoot 變數
-export DACS_codeRoot=/home/u1157393/FedLearn/
-export DACS_dataRoot=/home/u1157393/FedLearn/ADReSS-IS2020-data
+export DACS_codeRoot=/home/FedASR/dacs
+export DACS_dataRoot=/mnt/Internal/FedASR/Data/ADReSS-IS2020-data
 # 顯示 DACS_codeRoot 變數的值
 echo "DACS_codeRoot is set to: $DACS_codeRoot"
